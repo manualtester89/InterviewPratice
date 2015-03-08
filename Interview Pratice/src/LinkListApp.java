d24ec0bfefa60fc6c42bc1ed9d801b5b2465836f
nachiketnaik89 <nachiketnaik89@gmail.com>
2015-03-08 18:10:55 -0500

    third Commit

--- Interview Pratice/src/LinkListApp.java
+++ Interview Pratice/src/LinkListApp.java
@@ -157,7 +157,170 @@ public class LinkListApp{
    return null;
    
  }  
+ 
+ // insert into sorted linklist
+ public LinkList InsertList(LinkList head, LinkList insertThis){
+   if(head==null || head.getData()>insertThis.getData()){
+     insertThis.setNext(head);
+     return insertThis;
+   }
+   LinkList current = head;
+   LinkList prev = null;
+   while(current!=null && current.getData()<insertThis.getData()){
+     prev= current;
+     current= current.getNext();
+   }
+   prev.setNext(insertThis);
+   insertThis.setNext(current);
+   return head;
+ }
+ 
+ //Insert into sorted linklist
+ public LinkList insertLinkList(LinkList head, LinkList insertThis){
+   if(head==null || head.getData()>insertThis.getData()){
+     insertThis.setNext(head);
+     return insertThis;
+   }
+   LinkList current = head;
+   LinkList prev= null;
+   while(current!= null && current.getData()<insertThis.getData()){
+     prev = current;
+     current= current.getNext();
+   }
+   prev.setNext(insertThis);
+   insertThis.setNext(current);
+   return head;
+ }
+ 
+ //Reverse LinkList
+ public LinkList reverseLinkList(LinkList head){
+   if(head==null)return head;
+   LinkList current = head;
+   LinkList temp = null;
    
+   while(current!=null){
+     LinkList next = current.getNext();
+     current.setNext(temp);
+     temp= current;
+     current= next;
+   }
+   return temp;
+ }
+ 
+ // reverse linklist recursively
+ public LinkList reverseRecursively(LinkList head){
+   if(head==null)return head;
+   return reverseRecursivelyUtil(head, null);
+ }
+ 
+ public LinkList reverseRecursivelyUtil(LinkList head, LinkList next){
+   if(head==null)return next;
+   LinkList nextOne = head.getNext();
+   head.setNext(next);
+   return reverseRecursivelyUtil(nextOne, head);
+ }
+ 
+ //Find the joint node
+ public LinkList jointNode(LinkList head1, LinkList head2){
+   int l1 = Length(head1);
+   int l2 = Length(head2);
+   int diff = l1-l2;
+   if(diff<0){
+     LinkList temp = head2;
+     head2= head1;
+     head1=temp;
+     diff= -diff;
+   }
+   
+   while(diff<0){
+     head1= head1.getNext();
+     diff--;
+   }
+   while(head1 == null || head2==null){
+     if(head1==head2)return head1;
+     head1=head1.getNext();
+     head2= head2.getNext();
+   }
+   return null;
+ }
+ 
+ 
+ // Middle Element of LinkList
+ public LinkList middle(LinkList head){
+   LinkList first=head, second=head;
+   boolean flag = true;
+   while(first.getNext()!=null){
+     if(flag){
+       first= first.getNext();
+       flag = false;
+     }
+     else if(!flag){
+       first= first.getNext();
+       second= second.getNext();
+       flag=true;
+     }
+   }
+   return second;
+ }
+ 
+ //print LinkList in reverse Order
+ public void reverseOrder(LinkList head){
+   if(head==null){
+     return;
+   }
+   else{
+     reverseOrder(head.getNext());
+     System.out.println(head.getData());
+   }
+ }
+ 
+ //LinkList even or Odd
+ public int isLinkListOdd(LinkList head){
+   while(head!=null && head.getNext()!=null){
+     head= head.getNext().getNext();
+   }
+   if(head==null)return 0;
+   return 1;
+ }
+ 
+ //LinkList even or odd
+ public int isLinkListOddPratice(LinkList head){
+   while(head!=null && head.getNext()!=null){
+     head= head.getNext().getNext();
+   }
+   if(head == null)return 0;
+   return 1;
+ }
+ 
+ //Merge two LinkList
+ public LinkList MergeLinkList(LinkList A, LinkList B){
+   if(A== null)return B;
+   if(B==null)return A;
+   LinkList newNode;
+   if(A.getData()>B.getData()){
+     newNode = A;
+     newNode.setNext(MergeLinkList(A,B.getNext()));
+   }else{
+     newNode=B;
+     newNode.setNext(MergeLinkList(A.getNext(), B));
+   }
+   return newNode;
+ }
+ //LinkList merge
+ public LinkList Merge(LinkList A, LinkList B){
+   if(A==null)return B;
+   if(B==null)return A;
+   LinkList newNode;
+   if(A.getData()>B.getData()){
+     newNode=A;
+     newNode.setNext(Merge(A,B.getNext()));
+   }else{
+     newNode = B;
+     newNode.setNext(Merge(A.getNext(),B));
+   }
+   return newNode;
+ }
+ 
   
   
   
