088e64e474d9c2b8d68f8e92a6f31b0d4e5ac998
nachiketnaik89 <nachiketnaik89@gmail.com>
2015-03-09 18:58:58 -0500

    sixth Commit

--- Interview Pratice/src/BinaryTreeApp.java
+++ Interview Pratice/src/BinaryTreeApp.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTreeApp{
@@ -44,8 +46,95 @@ public class BinaryTreeApp{
   }
   
   
+  //preOrderIterative
+  public void preOrderIterative(BinaryTree root){
+    if(root==null)  return;
+    Stack<BinaryTree> st = new Stack<>();
+    while(true){
+      while(root!=null){
+        st.push(root);
+        System.out.println(root.getData());
+        root =root.getLeft();
+        
+      }
+      if(st.isEmpty()) break;
+      root= st.pop();
+      root= root.getRight();
+    }
+    
+  }
+  
+  //post Order Traversal
+  public void postOrderIterative(BinaryTree root){
+    if(root==null)return;
+    Stack<BinaryTree> st = new Stack<>();
+    while(true){
+      if(root!=null){
+        st.push(root);
+        root=root.getLeft();
+        
+      }else{
+        if(st.isEmpty()) break;
+        if(st.peek().getRight()==null){
+          root =st.pop();
+          System.out.println(root.getData());
+          if(root==st.peek().getRight()){
+            root=st.pop();
+            System.out.println(root.getData());
+          }
+          if(!st.isEmpty())
+          {
+            root =st.peek().getRight();
+          }else{
+            root=null;
+          }
+        }
+      }
+    }
+  }
   
   
+//pratice post order traversal  
+public void postOrderIterativePratice(BinaryTree root){
+ if(root==null)return;
+ Stack<BinaryTree> st  = new Stack<>();
+ while(true){
+   if(root!=null){
+     st.push(root);
+     root=root.getLeft();
+   }else{
+     if(st.isEmpty()) break;
+     if(st.peek().getRight()==null){
+       root= st.pop();
+       System.out.println(root.getData());
+       if(root== st.peek().getRight()){
+        root= st.pop();
+        System.out.println(root.getData());
+       }
+       if(!st.isEmpty()){
+         root = st.peek().getRight();
+       }else{
+         root=null;
+       }
+     }
+   }
+ }
+}
+ 
+ //Level order Traversal
+public void LevelOrderTraversal(BinaryTree root)  {
+ if(root==null)return;
+ Queue<BinaryTree> q = new LinkedList<BinaryTree>();
+ q.add(root);
+ while(!q.isEmpty()){
+   BinaryTree temp = q.poll();
+   System.out.println(temp.getData());
+   if(temp.getLeft()!=null)
+   q.add(temp.getLeft());
+   if(temp.getRight()!=null)
+   q.add(temp.getRight());
+ }
+}
   
   
   
